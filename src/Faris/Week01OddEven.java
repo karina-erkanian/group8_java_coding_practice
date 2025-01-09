479290b2c826efca4a660a0707848ef493d1ebef
fariselbattahe <fariselbattahe@gmail.com>
2025-01-09 17:09:20 -0500

    Just test Week01OddEven

---
+++ src/Faris/Week01OddEven.java
@@ -0,0 +1,14 @@
+package Faris;
+//Numbers -- odd & even
+//Write a method that can identify  a given number is even or odd.
+//
+//EX:
+//       identify(5) --> "Odd"
+//       identify(6) --> "Even
+public class Week01OddEven {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello word");
+    }
+}
