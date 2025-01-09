a44a86c6c34567fd83ed346eb14d8499d15317db
CyberLab <Marianikk122@hotmail.com>
2025-01-09 17:14:44 -0500

    The solution has been added

--- src/Masha/Week01_OddOr_Even.java
+++ src/Masha/Week01_OddOr_Even.java
@@ -2,11 +2,18 @@ package Masha;
 
 public class Week01_OddOr_Even {
 
+    public static String EvenOdd(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+
     public static void main(String[] args) {
+        int num = 7;
 
-
-        System.out.println("hello");
-
-
+        System.out.println(EvenOdd(num));
     }
 }
+
