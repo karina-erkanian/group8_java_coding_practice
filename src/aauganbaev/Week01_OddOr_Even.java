0714c427727bbd951cdf5d4c6e7495b95ffa2fa5
aauganbaev <aauganbaev@icloud.com>
2025-01-09 17:34:01 -0500

    updated by aauganbaev

---
+++ src/aauganbaev/Week01_OddOr_Even.java
@@ -0,0 +1,13 @@
+package aauganbaev;
+
+public class Week01_OddOr_Even {
+    //Numbers -- odd & even
+    //Write a method that can identify  a given number is even or odd.
+    //
+    //EX:
+    //       identify(5) --> "Odd"
+    //       identify(6) --> "Even"
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
