8b7c0373223f8d8d897262d46176b4d91ab0f1c1
aauganbaev <aauganbaev@icloud.com>
2025-01-14 23:52:07 -0500

    updated by aauganbaev!

---
+++ src/aauganbaev/FINRA.java
@@ -0,0 +1,17 @@
+package aauganbaev;
+
+public class FINRA {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
