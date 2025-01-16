03da50477d42a4dde2b80bcf8a47bcc4f8d2c59e
CyberLab <Marianikk122@hotmail.com>
2025-01-16 16:44:41 -0500

    Week 2 is done correctly

--- src/Masha/Week02_FINRA.java
+++ src/Masha/Week02_FINRA.java
@@ -6,12 +6,12 @@ public class Week02_FINRA {
 
 
         for (int i = 0; i <= 30; i++) {
-            if(i%3==0){
-                System.out.println("FIN");
+            if(i%3==0 && i%5==0){
+                System.out.println("FINRA");
             }else if(i%5==0){
                 System.out.println("RA");
-            }else if(i%3==0 && i%5==0){
-                System.out.println("FINRA");
+            }else if(i%3==0){
+                    System.out.println("FIN");
             }
         }
 
