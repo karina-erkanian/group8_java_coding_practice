2d4ce54ddd17ead2b68e7ada163cf4bea13ece2e
CyberLab <Marianikk122@hotmail.com>
2025-01-16 16:41:52 -0500

    Week 2 is done

---
+++ src/Masha/Week02_FINRA.java
@@ -0,0 +1,19 @@
+package Masha;
+
+public class Week02_FINRA {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <= 30; i++) {
+            if(i%3==0){
+                System.out.println("FIN");
+            }else if(i%5==0){
+                System.out.println("RA");
+            }else if(i%3==0 && i%5==0){
+                System.out.println("FINRA");
+            }
+        }
+
+    }
+}
