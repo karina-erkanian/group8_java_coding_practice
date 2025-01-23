6c7b9a8587dda8063dbb393bf0ebba5a2cca2ed9
isfarkhori7 <isfarkhori@gmail.com>
2025-01-23 15:33:10 -0500

    Week 2 is updated again.

--- src/isf/Week02_FINRA.java
+++ src/isf/Week02_FINRA.java
@@ -4,16 +4,8 @@ public class Week02_FINRA {
 
     public static void main(String[] args) {
 
-        FINRA(30);
-        System.out.println("-----------------------");
-        FINRA(56);
-
-    }
-
-    public static void FINRA(int num) {
-
+        int num = 30;
         for (int i = 1; i <= num; i++) {
-
             if (i % 15 == 0) {
                 System.out.println("FINRA");
             } else if (i % 3 == 0) {
@@ -22,6 +14,29 @@ public class Week02_FINRA {
                 System.out.println("RA");
             } else {
                 System.out.println(i);
+            }
+
+        }
+
+
+        FINRA(30);
+        System.out.println("-----------------------");
+        FINRA(56);
+
+    }
+
+    public static void FINRA(int num) {
+
+        for (int k = 1; k <= num; k++) {
+
+            if (k % 15 == 0) {
+                System.out.println("FINRA");
+            } else if (k % 3 == 0) {
+                System.out.println("FIN");
+            } else if (k % 5 == 0) {
+                System.out.println("RA");
+            } else {
+                System.out.println(k);
             }
 
         }
