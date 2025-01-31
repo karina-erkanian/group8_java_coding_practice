fb150ee2a306344f83f2fd55e962e1e681dad162
chedd <cheddadrachidpro@gmail.com>
2025-01-30 20:57:01 -0500

    OddOrEven is added

--- src/Rachid/OddOrEven.java
+++ src/Rachid/OddOrEven.java
@@ -1,8 +1,13 @@
 package Rachid;
 
 public class OddOrEven {
-    public void java(){
+    public static void main(String[] args) {
+        System.out.println(identify(9));
+        System.out.println(identify(10));
+    }
 
-
+    // Using a ternary operator for a concise solution
+    public static String identify(int number) {
+        return "Number " + number + " is " + (number % 2 == 0 ? "even" : "odd");
     }
 }
