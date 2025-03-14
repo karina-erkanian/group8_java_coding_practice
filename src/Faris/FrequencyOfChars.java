a5e5a7f4e6ea4ba5de657c2ac2a9c435b9786154
fariselbattahe <fariselbattahe@gmail.com>
2025-03-14 02:15:38 -0400

    Frequency of char and remove duplicate added.

---
+++ src/Faris/FrequencyOfChars.java
@@ -0,0 +1,11 @@
+package Faris;
+/*
+ ✅ String - Frequency of Characters
+
+         Write a return method that can find the frequency of characters
+
+         Ex: frequencyOfChars("AAABBCDD") ==> A3B2C1D2 😎
+
+ */
+public class FrequencyOfChars {
+}
