081bc6b9179b9dd87ac48b0a5b583aa3f22f42f7
karina-erkanian <carryk.erk@gmail.com>
2025-03-24 15:02:54 -0400

    Week 09 task is added.

---
+++ src/WeeklyTasks/Week08_ReverseString.java
@@ -0,0 +1,8 @@
+package WeeklyTasks;
+
+public class Week08_ReverseString {
+
+    /*
+    Write a return method that can reverse String
+     */
+}
---
+++ src/WeeklyTasks/Week09_PasswordValidation.java
@@ -0,0 +1,17 @@
+package WeeklyTasks;
+
+public class Week09_PasswordValidation {
+
+    /*
+    String -- Password Validation Task
+    1.Write a return method that can verify if a password is valid or not.
+    Requirements:
+    1.Password MUST be at least have 6 characters and should not contain spaces
+    2.Password should at least contain one upper case letter
+    3.Password should at least contain one lowercase letter
+    4.Password should at least contain one special character
+    5.Password should at least contain a digit
+
+    If all requirements above are met, the method returns true, otherwise returns false.
+     */
+}
